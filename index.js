3d5bdff8553a682fce0f58eb6cb14f55adbb5d6e
unknown <toledopabloesteban@gmail.com>
2020-02-24 17:12:38 -0300

    nada

--- index.js
+++ index.js
@@ -16,20 +16,13 @@ app.use( express.json({ extended: true}));
 
 
 //puerto de la app
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 //Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
-
-
-
-//definimos una pagina principal para probar el servidor
-/*app.get('/', (red, res) => {
-    res.send('Hola Mundo');
-});*/
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
