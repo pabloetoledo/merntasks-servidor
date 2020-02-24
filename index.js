72d162e6f969425d790f5c5db0676fdf85c7da41
unknown <toledopabloesteban@gmail.com>
2020-02-24 16:45:57 -0300

    correccion en el puerto

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ app.use('/api/tareas', require('./routes/tareas'));
     res.send('Hola Mundo');
 });*/
 
-app.listen(port, '0.0.0.0', () =>{
+app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
